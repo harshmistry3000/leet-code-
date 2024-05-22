8cc5faf4d53e8be08b4b05e05f14a36f837be283
HARSH MISTRY <115336217+harshmistry3000@users.noreply.github.com>
2024-05-22 17:11:23 +0530

    131_PalindromePartitioning.js

---
+++ 131_PalindromePartitioning.js
@@ -0,0 +1,42 @@
+/**
+ * @param {string} s
+ * @return {string[][]}
+ */
+
+var partition = function(s) {
+    const result = [];
+    
+
+    const isPalindrome = (str, left, right) => {
+        while (left < right) {
+            if (str[left] !== str[right]) return false;
+            left++;
+            right--;
+        }
+        return true;
+    };
+
+
+    const backtrack = (start, path) => {
+      
+        if (start === s.length) {
+            result.push([...path]);
+            return;
+        }
+        s
+        for (let end = start; end < s.length; end++) {
+            if (isPalindrome(s, start, end)) {
+                path.push(s.substring(start, end + 1)); 
+                backtrack(end + 1, path);              
+                path.pop();                            
+            }
+        }
+    };
+
+    backtrack(0, []);
+    return result;
+};
+
+
+console.log(partition("aab")); 
+console.log(partition("a"));   
